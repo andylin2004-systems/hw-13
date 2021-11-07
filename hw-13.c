6d4c4174e0d8e9ea8ad28c2db7e04ad88e0f9094
Andy Lin <60021592+andylin2004@users.noreply.github.com>
2021-11-06 21:01:24 -0400

    added the array of boros

--- hw-13.c
+++ hw-13.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <errno.h>
 #include <fcntl.h>
 #include <unistd.h>
@@ -18,28 +19,41 @@ int countLines(char* string){
         if (string[i] == '\n'){
             total++;
         }
+        i++;
     }
     return total;
 }
 
-void create(char* string){
+void createArray(char* string){
     int year, man, bk, qns, bx, si;
     int start = 0;
+    char line[100];
+    int lineCount = 0;
+    int lineTotal = countLines(string);
+    struct pop_entry *popInfo = malloc(sizeof(struct pop_entry)*countLines(string)*5);
     char *addrLineEnd = strchr(string, '\n');
     int length = addrLineEnd - string;
-    char line[100];
     while (addrLineEnd)
     {
         strncpy(line, string+start, length);
         line[length] = '\0';
         printf("%s\n", line);
-        sscanf(line, "%d,%d,%d,%d,%d,%d", &year, &man, &bk, &qns, &bx, &si);
-
+        sscanf(line, "%d,%d,%d,%d,%d,%d", &year, &(popInfo[lineCount*5].population), &(popInfo[lineCount*5+1].population), &(popInfo[lineCount*5+2].population), &(popInfo[lineCount*5+3].population), &(popInfo[lineCount*5+4].population));
+        popInfo[lineCount*5].year = year;
+        strcpy(popInfo[lineCount*5].boro, "Manhattan");
+        popInfo[lineCount*5+1].year = year;
+        strcpy(popInfo[lineCount*5+1].boro, "Brooklyn");
+        popInfo[lineCount*5+2].year = year;
+        strcpy(popInfo[lineCount*5+2].boro, "Queens");
+        popInfo[lineCount*5+3].year = year;
+        strcpy(popInfo[lineCount*5+3].boro, "Bronx");
+        popInfo[lineCount*5+4].year = year;
+        strcpy(popInfo[lineCount*5+4].boro, "Staten Island");
         start += length + 1;
         addrLineEnd = strchr(string + start, '\n');
         length = addrLineEnd - (string + start);
+        lineCount++;
     }
-    
 }
 
 int read_csv()
@@ -48,7 +62,7 @@ int read_csv()
     char doc[1000];
     FILE *file = open("nyc_pop.csv", 0644);
     read(file, doc, sizeof(doc));
-    create(doc);
+    createArray(doc);
 }
 
 int main(){
