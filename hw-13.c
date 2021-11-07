d4594ff1e069b87508e0013d22745ec7e5f66ae7
Andy Lin <60021592+andylin2004@users.noreply.github.com>
2021-11-06 21:07:30 -0400

    reindent line, now includes last line data

--- hw-13.c
+++ hw-13.c
@@ -5,18 +5,21 @@
 #include <unistd.h>
 #include <string.h>
 
-struct pop_entry{
+struct pop_entry
+{
     int year;
     int population;
     char boro[15];
 };
 
-int countLines(char* string){
+int countLines(char *string)
+{
     int i = 0;
     int total = 1;
     while (string[i])
     {
-        if (string[i] == '\n'){
+        if (string[i] == '\n')
+        {
             total++;
         }
         i++;
@@ -24,36 +27,49 @@ int countLines(char* string){
     return total;
 }
 
-void createArray(char* string){
+struct pop_entry *createArray(char *string)
+{
     int year, man, bk, qns, bx, si;
     int start = 0;
     char line[100];
     int lineCount = 0;
     int lineTotal = countLines(string);
-    struct pop_entry *popInfo = malloc(sizeof(struct pop_entry)*countLines(string)*5);
+    struct pop_entry *popInfo = malloc(sizeof(struct pop_entry) * countLines(string) * 5);
     char *addrLineEnd = strchr(string, '\n');
     int length = addrLineEnd - string;
     while (addrLineEnd)
     {
-        strncpy(line, string+start, length);
+        strncpy(line, string + start, length);
         line[length] = '\0';
-        printf("%s\n", line);
-        sscanf(line, "%d,%d,%d,%d,%d,%d", &year, &(popInfo[lineCount*5].population), &(popInfo[lineCount*5+1].population), &(popInfo[lineCount*5+2].population), &(popInfo[lineCount*5+3].population), &(popInfo[lineCount*5+4].population));
-        popInfo[lineCount*5].year = year;
-        strcpy(popInfo[lineCount*5].boro, "Manhattan");
-        popInfo[lineCount*5+1].year = year;
-        strcpy(popInfo[lineCount*5+1].boro, "Brooklyn");
-        popInfo[lineCount*5+2].year = year;
-        strcpy(popInfo[lineCount*5+2].boro, "Queens");
-        popInfo[lineCount*5+3].year = year;
-        strcpy(popInfo[lineCount*5+3].boro, "Bronx");
-        popInfo[lineCount*5+4].year = year;
-        strcpy(popInfo[lineCount*5+4].boro, "Staten Island");
+        sscanf(line, "%d,%d,%d,%d,%d,%d", &year, &(popInfo[lineCount * 5].population), &(popInfo[lineCount * 5 + 1].population), &(popInfo[lineCount * 5 + 2].population), &(popInfo[lineCount * 5 + 3].population), &(popInfo[lineCount * 5 + 4].population));
+        popInfo[lineCount * 5].year = year;
+        strcpy(popInfo[lineCount * 5].boro, "Manhattan");
+        popInfo[lineCount * 5 + 1].year = year;
+        strcpy(popInfo[lineCount * 5 + 1].boro, "Brooklyn");
+        popInfo[lineCount * 5 + 2].year = year;
+        strcpy(popInfo[lineCount * 5 + 2].boro, "Queens");
+        popInfo[lineCount * 5 + 3].year = year;
+        strcpy(popInfo[lineCount * 5 + 3].boro, "Bronx");
+        popInfo[lineCount * 5 + 4].year = year;
+        strcpy(popInfo[lineCount * 5 + 4].boro, "Staten Island");
         start += length + 1;
         addrLineEnd = strchr(string + start, '\n');
         length = addrLineEnd - (string + start);
         lineCount++;
     }
+    strcpy(line, string + start);
+    sscanf(line, "%d,%d,%d,%d,%d,%d", &year, &(popInfo[lineCount * 5].population), &(popInfo[lineCount * 5 + 1].population), &(popInfo[lineCount * 5 + 2].population), &(popInfo[lineCount * 5 + 3].population), &(popInfo[lineCount * 5 + 4].population));
+    popInfo[lineCount * 5].year = year;
+    strcpy(popInfo[lineCount * 5].boro, "Manhattan");
+    popInfo[lineCount * 5 + 1].year = year;
+    strcpy(popInfo[lineCount * 5 + 1].boro, "Brooklyn");
+    popInfo[lineCount * 5 + 2].year = year;
+    strcpy(popInfo[lineCount * 5 + 2].boro, "Queens");
+    popInfo[lineCount * 5 + 3].year = year;
+    strcpy(popInfo[lineCount * 5 + 3].boro, "Bronx");
+    popInfo[lineCount * 5 + 4].year = year;
+    strcpy(popInfo[lineCount * 5 + 4].boro, "Staten Island");
+    return popInfo;
 }
 
 int read_csv()
@@ -62,9 +78,10 @@ int read_csv()
     char doc[1000];
     FILE *file = open("nyc_pop.csv", 0644);
     read(file, doc, sizeof(doc));
-    createArray(doc);
+    struct pop_entry *boroArray = createArray(doc);
 }
 
-int main(){
+int main()
+{
     read_csv();
 }
